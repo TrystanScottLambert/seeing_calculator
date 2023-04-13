921fda28c99573f2dd793182d4c41994fa420a25
Trystan Lambert <trystanscottlambert@gmail.com>
2023-04-13 11:38:38 -0400

    Added FWHM measurement and test

---
+++ extract_seeing_profile.py
@@ -0,0 +1,35 @@
+"""
+Module to perform aperture photometry on a single stellar source. 
+I.e., place several annuli over a 2d source and extract a profile. 
+"""
+
+import numpy as np
+from astropy.modeling import models, fitting
+
+def fit_2d_gauss(data: np.ndarray):
+    """
+    Models a 2 Gaussian and fits it to the cut out source.
+    The source has to be a stamp cut out. Function cannot be
+    used on a full astronomy image.
+    """
+    fit = fitting.LevMarLSQFitter()
+    # Initial Guesses based on the data
+    y_0, x_0 = np.unravel_index(np.argmax(data), data.shape)
+    sigma = np.std(data)
+    amp = np.max(data)
+
+    model = models.Gaussian2D(amp, x_0, y_0, sigma, sigma)
+
+    y_i, x_i = np.indices(data.shape)
+
+    fitted_model = fit(model, x_i, y_i, data)
+    return fitted_model
+
+def get_average_fwhm(data: np.ndarray):
+    """
+    Determines the average standard deviation of the fitted 2D Gaussian
+    and then converts this into FWHM
+    """
+    gaussian_2d = fit_2d_gauss(data)
+    avg_fwhm = np.mean([gaussian_2d.x_fwhm, gaussian_2d.y_fwhm])
+    return avg_fwhm
---
+++ test_extract_seeing_profile.py
@@ -0,0 +1,52 @@
+"""
+Testing module for extraction seeing profiles.
+"""
+
+from typing import Tuple
+import unittest
+import numpy as np
+from astropy.table import Table
+from photutils.datasets import make_gaussian_sources_image
+
+import extract_seeing_profile
+
+
+# Creating a test image
+def create_test_cutout(std: float) -> Tuple[np.ndarray, float]:
+    """
+    Creates a false star cutout for testing.
+    Only free parameter is the std since this is the 
+    only important parameter that needs to be recovered.
+    """
+    sources = Table()
+    sources['flux'] = [10000]
+    sources['x_mean'] = [30]
+    sources['y_mean'] = [30]
+    sources['x_stddev'] = [std]
+    sources['y_stddev'] = sources['x_stddev']
+    sources['theta'] = [0]
+    tshape = (60, 60)
+    image = (make_gaussian_sources_image(tshape, sources)
+)
+    return image, std
+
+
+class TestExtractSeeingProfile(unittest.TestCase):
+    """
+    Testing class for the extract_seeing_profile module.
+    """
+
+    def test_fit2d_gauss(self):
+        """
+        Testing if the 2d gaussian function is working and recovering several different 
+        fake stellar sources.
+        """
+        test_cutouts = [create_test_cutout(std) for std in np.arange(2, 10, 0.1)]
+        for test_cutout in test_cutouts:
+            fit = extract_seeing_profile.fit_2d_gauss(test_cutout[0])
+            fit.x_stddev.value, test_cutout[1]
+            self.assertAlmostEqual(fit.x_stddev.value, test_cutout[1])
+    
+
+if __name__ == '__main__':
+    unittest.main()
