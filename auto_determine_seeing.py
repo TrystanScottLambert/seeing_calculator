b8c0daaf9bf2280045fef6d168a9aa605476ce6a
Trystan Lambert <trystanscottlambert@gmail.com>
2023-05-26 17:43:01 -0400

    auto working

--- auto_determine_seeing.py
+++ auto_determine_seeing.py
@@ -4,10 +4,13 @@ Automatically estimating the seeing.
 
 import numpy as np
 from astropy.io import fits
-from astropy.stats import gaussian_fwhm_to_sigma
-
+from astropy.stats import SigmaClip, sigma_clipped_stats
+from photutils.background import Background2D, MedianBackground
 from source_identification import find_appropriate_sources
 from extract_seeing_profile import get_average_fwhm
+
+sigma_clip = SigmaClip(sigma=3.0)
+bkg_estimator = MedianBackground()
 
 
 BOX_WIDTH = 15 # pixels
@@ -21,9 +24,9 @@ def stamp_cut_out(data: np.ndarray, x_pos:float, y_pos:float) -> np.ndarray:
         int(y_pos)-BOX_WIDTH:int(y_pos)+BOX_WIDTH, int(x_pos)-BOX_WIDTH:int(x_pos)+BOX_WIDTH]
     return cut_out
 
-def calc_seeing(data: np.ndarray, fwhm:float, std_above_background:float) -> list[float]:
+def calc_seeing(data: np.ndarray, fwhm:float, min_val:float, plot: bool = False) -> float:
     """Returns a list of fwhm measurements for in pixels."""
-    sources = find_appropriate_sources(data, fwhm, std_above_background)
+    sources = find_appropriate_sources(data, fwhm, min_val, plot)
     x_positions = sources['xcentroid'].value
     y_positions = sources['ycentroid'].value
     fwhms = []
@@ -33,12 +36,37 @@ def calc_seeing(data: np.ndarray, fwhm:float, std_above_background:float) -> lis
             fwhms.append(get_average_fwhm(postage_stamp))
         except:
             pass
-    return np.mean(fwhms) * gaussian_fwhm_to_sigma
+
+    fwhms = np.array(fwhms)
+    fwhms = fwhms[(fwhms>0.5) & (fwhms<20)]  # More than 20 pixels is just unphysical
+    return sigma_clipped_stats(fwhms)[1]
 
 
 if __name__ == '__main__':
-    INFILE = '../DECAM_analysis/correct_stacks/N964/n964.fits'
-    #INFILE = '/home/tlambert/Desktop/IMACS_photometry/imacs_data/night_1_theli.fits'
-    hdu = fits.open(INFILE)
-    decam_n_seeing = calc_seeing(hdu[0].data, fwhm=7.5, std_above_background=25)
-    print(decam_n_seeing)
+    import pylab as plt
+    from astropy.visualization import ZScaleInterval
+    INFILES = [
+    #'../IMACS_photometry/imacs_data/night_1_theli.fits',
+    #'../IMACS_photometry/imacs_data/night_2_theli.fits',
+    '../DECAM_analysis/correct_stacks/N964/z.fits',
+    '../DECAM_analysis/correct_stacks/N964/.fits',
+    '../DECAM_analysis/correct_stacks/N964/z.fits',
+    ]
+
+    for file in INFILES:
+        hdu = fits.open(file)
+        len_y, len_x = hdu[0].data.shape
+        center_y = int(len_y/2)
+        center_x = int(len_x/2)
+        zscale = ZScaleInterval()
+        v_min, v_max = zscale.get_limits(hdu[0].data)
+        PAD = 6000
+        data = hdu[0].data[center_y-PAD: center_y+PAD, center_x-PAD: center_x+PAD]
+        bkg = Background2D(data, data.shape, filter_size=(3, 3),
+                   sigma_clip=sigma_clip, bkg_estimator=bkg_estimator)
+        data -= bkg.background
+        plt.imshow(data)
+        zscale = ZScaleInterval()
+        v_min, v_max = zscale.get_limits(data)
+        seeing = calc_seeing(data, fwhm=5, min_val=10)
+        print(file.split('/')[-1], seeing)
--- source_identification.py
+++ source_identification.py
@@ -3,32 +3,70 @@ Module for identiyfing non saturated sources in the image.
 """
 
 import numpy as np
+import matplotlib.pyplot as plt
 from astropy.table import Table
 from astropy.stats import sigma_clipped_stats
+from astropy.visualization import ZScaleInterval
 from photutils.detection import IRAFStarFinder
+from photutils.aperture import CircularAperture
 
-def find_sources(data: np.ndarray, fwhm: float, std_above_background: float) -> Table:
+
+def find_center(image: np.ndarray, factor: float) -> np.ndarray:
+    """
+    Returns a mask representing the 1./factor central region of the image.
+    """
+
+    height, width = image.shape
+    center_x = int(image.shape[1]/2)
+    center_y = int(image.shape[0]/2)
+    box_height = int(height/factor)
+    box_width = int(width/factor)
+
+    mask = np.zeros(image.shape, dtype=bool)
+    mask[:,:] = True
+    mask[center_y-box_height:center_y + box_height,center_x-box_width: center_x+box_width] = 0.
+    return mask
+
+def find_sources(
+        data: np.ndarray, fwhm: float, min_val: float, central_factor: float = 4.
+        ) -> Table:
     """Identifies the bright sources in an image."""
-    _, median, std = sigma_clipped_stats(data)
-    daofind = IRAFStarFinder(fwhm=fwhm, threshold=std_above_background*std)
-    sources = daofind(data - median)
+    _, median, _ = sigma_clipped_stats(data)
+    daofind = IRAFStarFinder(fwhm=fwhm, threshold=min_val)
+    if central_factor:
+        msk = find_center(data, central_factor)
+        sources = daofind(data - median, mask=msk)
+    else:
+        sources = daofind(data - median)
     return sources
 
 def select_non_saturated_sources(source_table: Table):
     """
     Determines the sources that are not saturated and are
-    not too dim in order to determine the fwhm. To do this
-    we take the values between median + median/2 to median.
+    not too dim in order to determine the fwhm. 
     """
     fluxes = source_table['flux'].value
     median = np.median(fluxes)
     cut = np.where(fluxes < 2*median)[0]
     return cut
 
-def find_appropriate_sources(data: np.ndarray, fwhm: float, std_above_background: float) -> Table:
+def find_appropriate_sources(
+        data: np.ndarray, fwhm: float, min_val: float,
+        central_factor: float = 4, plot=False
+        ) -> Table:
     """
     Returns only good sources in a given image.
     """
-    all_sources = find_sources(data, fwhm, std_above_background)
+    all_sources = find_sources(data, fwhm, min_val, central_factor=central_factor)
     idx_good = select_non_saturated_sources(all_sources)
-    return all_sources[idx_good]
+    good_sources = all_sources[idx_good]
+
+    if plot:
+        positions = np.transpose((good_sources['xcentroid'], good_sources['ycentroid']))
+        apertures = CircularAperture(positions, r=4.0)
+        zscale = ZScaleInterval()
+        v_min, v_max = zscale.get_limits(data)
+        plt.imshow(data, cmap='Greys', vmin=v_min, vmax=v_max)
+        apertures.plot(color='blue', lw=1.5, alpha=0.5)
+        plt.show()
+    return good_sources
